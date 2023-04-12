822dc444c327cb3eb9f22d36caf0a7b943b535dd
bestaford <butenko1demitr@gmail.com>
2023-04-12 15:26:14 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import calendar
+import os
+import sys
+import datetime
+
+
+def main():
+    args = sys.argv
+    args.pop(0)
+    for path in args:
+        cleanup(path)
+
+
+def cleanup(path):
+    for filename in os.listdir(path):
+        file = path + "/" + filename
+        if os.path.isfile(file):
+            backup_date = get_date(filename)
+            last_month_day = calendar.monthrange(backup_date.year, backup_date.month)[1]
+            now = datetime.datetime.now().date()
+            if backup_date <= (now - datetime.timedelta(days=10)) and backup_date.day != last_month_day:
+                print(file)
+
+
+def get_date(filename):
+    part1 = filename[filename.index("_") + 1:]
+    part2 = part1[:part1.index(".")]
+    return datetime.datetime.strptime(part2, "%d_%m_%Y").date()
+
+
+if __name__ == "__main__":
+    main()
